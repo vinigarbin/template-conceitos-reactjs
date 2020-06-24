8b52c58e752293e9d78eb5e7a8dce91b39c59531
Vinicius Garbin <vinigarbin@gmail.com>
2020-06-24 07:36:05 -0300

    Ajustando metodos para consumir a API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const resp = await api.post('repositories', {
+      title: `Novo repositorio Desafio${Date.now()}`,
+      url: "https://github.com/vinigarbin",
+      techs: [
+        "React"
+      ]
+    });
+
+    const repo = resp.data;
+
+    setRepositories([...repositories, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(repo => repo.id !== id))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repo => <li key={repo.id}>{repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+              </button></li>)
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
